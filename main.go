75f7ad2e6ec7074d86dde628f4fbc58ba7b440b8
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-14 15:27:57 +0900

    :+1: method2

--- main.go
+++ main.go
@@ -9,11 +9,26 @@ type Vertex struct {
 	X, Y float64
 }
 
+type MyFloat float64
+
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
 	v := Vertex{X: 3, Y: 4}
 	fmt.Println(v.Abs())
+
+	f := MyFloat(3.0)
+	fmt.Println(f.Abs())
+
+	f2 MyFloat := -3.0
+	fmt.Println(f2)
+
 }
