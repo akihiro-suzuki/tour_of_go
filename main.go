67fa4d520e0cf71918c600f0528cff5db0d754ab
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-15 16:15:54 +0900

    型アサーション

--- main.go
+++ main.go
@@ -1,31 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = &f // a *MyFloat implements Abser
-	a = &v // a *Vertex implements Abser
-
-	a = &v
-
-	fmt.Println(a.Abs())
-	describe(a)
+// q: X, Yのデフォルト値を設定しておきたい
+type MyStruct struct {
+	X, Y int
 }
 
-func describe(a Abser) {
-	// %T はtype
-	// %v はvalue
-	writtenBytes, err := fmt.Printf("(%v, %T)\n", a, a)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(writtenBytes)
-	}
+func main() {
+	var i interface{} = "hello"
+
+	m, ok := i.(MyStruct) // {0, 0}, false
+	fmt.Println(m, ok)
+
+	m2 := i.(MyStruct) // panic
+	fmt.Println(m2)
 }
