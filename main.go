b8bfab9a17ab35fba3007afbeb4d1343ad144016
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-15 05:52:25 +0000

    before interface.go

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Vertex struct {
 	X, Y float64
 }
 
 func (v Vertex) Abs() float64 {
-	return v.X*v.X + v.Y*v.Y
+	// public メソッドは大文字始まり
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-// q: * つけるとmutableになるという理解でいいのかな
-// a: そうですね。ポインタを使うと、呼び出し元の値を更新できます。
-// q: & をつける例はありますか？
-// a: あまりないですね。ポインタを使うときは、呼び出し元の値を更新したいときに使います。
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(10)
-	fmt.Println(v.Abs())
+	fmt.Println((&v).Abs())
+	fmt.Println(AbsFunc(v))
 }
