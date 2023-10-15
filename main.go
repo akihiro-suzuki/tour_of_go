dfa1ee3b8aa43ab55da87bbf89c91900acb9717f
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-15 07:35:54 +0000

    empty list to add values

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-import "fmt"
-
-type Person struct {
-	Name string
-	Age  int
-}
-
-// Stringer is an interface type that describes objects that can describe themselves as strings.
-func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	a := Person{"ABC", 42}
-	b := Person{"DEF", 9001}
-	fmt.Println(a, b)
+	var list []string = nil
+	fmt.Printf("list: %v\n", list)
+	for i := 0; i < 10; i++ {
+		list = append(list, strconv.Itoa(i))
+	}
+	fmt.Println(list)
 }
