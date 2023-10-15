fb3fe06b6bb74c9ee8ff699097764fc764fe2c99
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-15 07:20:47 +0000

    型スイッチ

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-// q: X, Yのデフォルト値を設定しておきたい
-type MyStruct struct {
-	X, Y int
+func do(i interface{}) {
+	switch t := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", t, t*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", t, len(t))
+	default:
+		fmt.Printf("I don't know about type %T!\n", t)
+	}
 }
 
 func main() {
-	var i interface{} = "hello"
-
-	m, ok := i.(MyStruct) // {0, 0}, false
-	fmt.Println(m, ok)
-
-	m2 := i.(MyStruct) // panic
-	fmt.Println(m2)
+	do(21)
+	do("hello")
+	do(true)
+	var i interface{} = nil
+	do(i)
 }
