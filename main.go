f85b61294d8e6ffe6badd11e60a78682a8fd0db7
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-20 15:21:39 +0000

    aa

--- main.go
+++ main.go
@@ -1,32 +1,24 @@
 package main
 
-import "fmt"
-
-// select は、複数ある case のいずれかが準備できるようになるまでブロックし、
-// 準備ができた case を実行します。 もし、複数の case の準備ができている場合、 case はランダムに選択されます。
-// 両方チャネル
-func fibonacci(c, quit chan int) {
-	x, y := 0, 1
-	for {
-		select {
-		case c <- x:
-			x, y = y, x+y
-		case <-quit:
-			fmt.Println("quit")
-			return
-		}
-	}
-
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	ch := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-ch)
+	// q: when this tick channel fired?
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Microsecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
 		}
-		quit <- 0
-	}()
-	fibonacci(ch, quit)
+	}
 }
