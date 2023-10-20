09fcba4ecdb4270c95e9fc8d1d5c5df45997313b
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-20 15:23:08 +0000

    あｓｄふぁｄ

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
 
 func main() {
 	// q: when this tick channel fired?
+	// tickは毎回なる(every 100msec)
 	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Microsecond)
+	// Afterは１回だけ
+	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
 		case <-tick:
 			fmt.Println("tick.")
 		case <-boom:
 			fmt.Println("BOOM!")
-			return
 		default:
 			fmt.Println("    .")
 			time.Sleep(50 * time.Millisecond)
