1de17842dcecf29065cb273cfa58be2dc5695b7b
akihiro_suzuki <akihiro_suzuki@just-ltd.co.jp>
2023-10-20 15:10:54 +0000

    channmel select

--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import "fmt"
 
-func fibonacci(n int, c chan int) {
+// select は、複数ある case のいずれかが準備できるようになるまでブロックし、
+// 準備ができた case を実行します。 もし、複数の case の準備ができている場合、 case はランダムに選択されます。
+// 両方チャネル
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
 	}
-	// コメントアウトすると、rangeでdeadlockになる
-	close(c)
+
 }
 
 func main() {
-	c := make(chan int, 5)
-	fmt.Println(cap(c))
-	go fibonacci(cap(c), c)
-
-	// closeすると、rangeが終了する
-	for i := range c {
-		fmt.Println(i)
-	}
-	fmt.Println("done")
+	ch := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
+		}
+		quit <- 0
+	}()
+	fibonacci(ch, quit)
 }
